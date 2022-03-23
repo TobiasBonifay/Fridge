a83efd7d6dcde8a4439f123188a5c7796a5f6c74
Aymaneelfiguigui <90783893+Aymaneelfiguigui@users.noreply.github.com>
2022-03-23 01:02:23 +0100

    [DEV] implementing google maps

--- app/src/main/java/edu/polytech/fridge/MainActivity.java
+++ app/src/main/java/edu/polytech/fridge/MainActivity.java
@@ -2,9 +2,18 @@ package edu.polytech.fridge;
 
 import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -12,7 +21,8 @@ import androidx.navigation.ui.NavigationUI;
 
 import edu.polytech.fridge.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+    GoogleMap map;
 
     private ActivityMainBinding binding;
 
@@ -34,6 +44,16 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        map = googleMap;
+        LatLng Nice =  new LatLng(43.681035, 7.224034);
+        map.addMarker(new MarkerOptions().position(Nice).title("Nice"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(Nice));
+
+    }
 }
