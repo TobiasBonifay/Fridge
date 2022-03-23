f0bb0a7756460a84cf16a4743d7cd1aa9c931987
TobiasBonifay <tobias.bonifay@etu.univ-cotedazur.fr>
2022-03-23 13:56:35 +0100

    Extracting navigation bar as a private method

--- app/src/main/java/edu/polytech/fridge/MainActivity.java
+++ app/src/main/java/edu/polytech/fridge/MainActivity.java
@@ -25,6 +25,10 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        setUpBottomNavigationBar();
+    }
+
+    private void setUpBottomNavigationBar() {
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
