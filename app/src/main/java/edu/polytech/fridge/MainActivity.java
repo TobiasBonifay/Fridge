2a533ce7eb4a1ff1749a591008f21df45819dfb6
TobiasBonifay <tobias.bonifay@etu.univ-cotedazur.fr>
2022-03-23 13:46:41 +0100

    Migrated fragment to androidx.fragment.app.FragmentContainerView for optimisations issues

--- app/src/main/java/edu/polytech/fridge/MainActivity.java
+++ app/src/main/java/edu/polytech/fridge/MainActivity.java
@@ -2,13 +2,15 @@ package edu.polytech.fridge;
 
 import android.os.Bundle;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.Objects;
 
 import edu.polytech.fridge.databinding.ActivityMainBinding;
 
@@ -31,7 +33,10 @@ public class MainActivity extends AppCompatActivity {
                 R.id.navigation_recipe,
                 R.id.navigation_notifications)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment_activity_main);
+        NavController navController = Objects.requireNonNull(navHostFragment).getNavController();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
