cdca635ba9adb782f5dc29d0b46d39500370c407
TobiasBonifay <tobias.bonifay@etu.univ-cotedazur.fr>
2022-03-30 16:58:18 +0200

    image notification working but not optimized

--- app/src/main/java/edu/polytech/fridge/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/edu/polytech/fridge/ui/notifications/NotificationsFragment.java
@@ -1,11 +1,15 @@
 package edu.polytech.fridge.ui.notifications;
 
+import android.annotation.SuppressLint;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,8 +22,13 @@ import androidx.core.app.NotificationCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Objects;
 import java.util.Random;
+import java.util.logging.Logger;
 
 import edu.polytech.fridge.MainActivity;
 import edu.polytech.fridge.R;
@@ -41,7 +50,7 @@ public class NotificationsFragment extends Fragment {
         notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
 
         final Button showNotificationButton = binding.showNotificationExample;
-        showNotificationButton.setOnClickListener(view -> showNotificationTextOnly("text only"));
+        showNotificationButton.setOnClickListener(view -> showNotificationImage());
 
         return root;
     }
@@ -95,9 +104,9 @@ public class NotificationsFragment extends Fragment {
         Objects.requireNonNull(notificationManager).notify(notificationId, notification);
     }
 
-    private void showNotificationWithImage(final String text) {
+    private void showNotificationWithImage(final Bitmap img, final String text) {
         final int notificationId = new Random().nextInt(100);
-        String channelId = "notification.channel";
+        String channelId = "notification.channel2";
 
         NotificationManager notificationManager = (NotificationManager) requireActivity()
                 .getSystemService(Context.NOTIFICATION_SERVICE);
@@ -125,13 +134,14 @@ public class NotificationsFragment extends Fragment {
         builder.setContentTitle("FRIDGE");
         builder.setContentText(text);
         builder.setContentIntent(pendingIntent);
+        builder.setStyle(new NotificationCompat.BigPictureStyle().bigPicture(img));
         builder.setAutoCancel(true);
         builder.setPriority(NotificationCompat.PRIORITY_HIGH);
 
         if (notificationManager != null && notificationManager.getNotificationChannel(channelId) == null) {
             NotificationChannel notificationChannel = new NotificationChannel(
                     channelId,
-                    "notification.channel",
+                    "notification.channel2",
                     NotificationManager.IMPORTANCE_HIGH);
 
             notificationChannel.setDescription("Notif channel to notify user");
@@ -142,6 +152,34 @@ public class NotificationsFragment extends Fragment {
 
         Notification notification = builder.build();
         Objects.requireNonNull(notificationManager).notify(notificationId, notification);
+    }
+
+
+    @SuppressLint("StaticFieldLeak")
+    private void showNotificationImage() {
+        new AsyncTask<String, Void, Bitmap>() {
+            @Override
+            protected Bitmap doInBackground(String... strings) {
+                InputStream inputStream;
+                try {
+                    URL url = new URL(strings[0]);
+                    HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                    httpURLConnection.setDoInput(true);
+                    httpURLConnection.connect();
+                    inputStream = httpURLConnection.getInputStream();
+                    return BitmapFactory.decodeStream(inputStream);
+                } catch (Exception e) {
+                    Logger.getLogger("notif", "streamerror");
+                    return null;
+                }
+            }
+
+            @Override
+            protected void onPostExecute(Bitmap bitmap) {
+                super.onPostExecute(bitmap);
+                showNotificationWithImage(bitmap, "testing");
+            }
+        }.execute("https://www.onceuponachef.com/images/2011/11/potato-leek-soup-14.jpg");
     }
 
     @Override
