54b034772bab73766c4dd8d1eb0b4872b6cbcefc
SAISSIOMAR <omar.saissi@etu.univ-cotedazur.fr>
2022-04-12 19:32:38 +0200

    add intent to btn 1

--- app/src/main/java/edu/polytech/fridge/MainActivity.java
+++ app/src/main/java/edu/polytech/fridge/MainActivity.java
@@ -1,33 +1,28 @@
 package edu.polytech.fridge;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import com.google.android.gms.maps.CameraUpdate;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-import java.util.ArrayList;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import edu.polytech.fridge.databinding.ActivityMainBinding;
+import edu.polytech.fridge.map.MapActivity;
 
 public class MainActivity extends AppCompatActivity {
     GoogleMap map;
+    Button btn;
+    Intent intent;
     private ListView listView;
 
     private ActivityMainBinding binding;
@@ -48,6 +43,15 @@ public class MainActivity extends AppCompatActivity {
                 R.id.navigation_recipe,
                 R.id.navigation_notifications)
                 .build();
+        btn=findViewById(R.id.butt1);
+        intent = new Intent(MainActivity.this, MapActivity.class);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(intent);
+            }
+        });
+
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
