f28b16d3e6b5878c5c9ed5cdd9dd62cece255edf
Aymaneelfiguigui <aymane.elfiguigui@gmail.com>
2022-04-23 18:35:03 +0200

    [DEV] add pictures taken from camera

--- app/src/main/java/edu/polytech/fridge/DonationsFragement.java
+++ app/src/main/java/edu/polytech/fridge/DonationsFragement.java
@@ -1,20 +1,64 @@
 package edu.polytech.fridge;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 
 public class DonationsFragement extends Fragment {
+    ImageView imageView;
+    Button button;
+
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_donations_fragement, container, false);
+        View view = inflater.inflate(R.layout.fragment_donations_fragement, container, false);
+        imageView = view.findViewById(R.id.picture);
+        button = view.findViewById(R.id.openCamera);
+
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions( getActivity(),
+                    new String[]{Manifest.permission.CAMERA},  101);
+
+
+        }
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,101);
+
+
+            }
+        });
+
+
+        return view;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode,resultCode,data);
+        if(requestCode == 101){
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(bitmap);
+        }
     }
 }
